a5769a6a54e8f051c3bd1ba1e8f5055634121170
unknown <vcianya>
2018-05-11 17:37:06 -0400

    http-server

---
+++ gps-tracking/http-server.js
@@ -0,0 +1,21 @@
+'use-strict'
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/probando', (req,res)=>{
+    res.status(200).send({message:'Este es el metodo probando'});
+});
+
+var http_server = require('http').Server(app);
+var io = require('socket.io')(http_server);
+io.on('connection', (socket)=>{
+    console.log('algien se a connectado');
+});
+http_server.emmit_socket = (data)=>{
+    io.sockets.emit('messages', data);
+    console.log(data);
+    
+}
+module.exports = http_server;
--- gps-tracking/server.js
+++ gps-tracking/server.js
@@ -1,5 +1,5 @@
 var gps = require("./../gps-tracking");
-
+var app = require('./http-server');
 var options = {
     'debug'                 : true,
     'port'                  : 5000,
@@ -7,13 +7,11 @@ var options = {
 }
 
 var server = gps.server(options,function(device,connection){
-
+    device.on("connected", function (data) {
+        console.log("Soy un nuevo dispositivo queriendo conectarme");
+    });
     device.on("login_request",function(device_id,msg_parts){
-
-        // Some devices sends a login request before transmitting their position
-        // Do some stuff before authenticate the device... 
-        
-        // Accept the login request. You can set false to reject the device.
+        console.log('¡Oye! Quiero comenzar a transmitir mi posición. Por favor Acéptame. Me llamo ' + device_id);
         this.login_authorized(true); 
 
     });
@@ -21,8 +19,7 @@ var server = gps.server(options,function(device,connection){
 
     //PING -> When the gps sends their position  
     device.on("ping",function(data){
-
-        //After the ping is received, but before the data is saved
+        app.emmit_socket({latitude:data.latitude, longitude:data.longitude});
         console.log(data);
         return data;
 
@@ -31,4 +28,8 @@ var server = gps.server(options,function(device,connection){
         console.log("Connection Error Victor: ");
         console.log(data);
     });
+});
+//HTPP SERVER
+var serv=app.listen(3000,()=>{
+    console.log("EL SERVIDOR LOCAL CON NODE Y EXPRESS ESTA CORRIENDO");        
 });
